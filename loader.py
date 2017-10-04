7bc78f8d07cc4a76584b6e43b82cca86b17de51c
Leonardo Arcari <lippol94@gmail.com>
2017-10-04 13:12:58 +0200

    Initial implementation of Global Effect recommendation

--- loader.py
+++ loader.py
@@ -1,4 +1,5 @@
 import csv
+from scipy.sparse import lil_matrix
 
 
 class Dataset():
@@ -9,10 +10,20 @@ class Dataset():
         self.tracks_final = load_csv(prefix + 'tracks_final.csv', 'track_id')
         self.playlists_final = load_csv(prefix + 'playlists_final.csv',
                                         'playlist_id')
-        self.train_final = load_csv(prefix + 'train_final.csv', 'playlist_id')
         self.target_playlists = load_csv(prefix + 'target_playlists.csv',
                                          'playlist_id')
         self.target_tracks = load_csv(prefix + 'target_tracks.csv', 'track_id')
+        self.train_final = load_train_final(prefix + 'train_final.csv')
+
+
+def load_train_final(path):
+    res = {}
+    with open(path, newline='') as csv_file:
+        reader = csv.DictReader(csv_file, delimiter='\t')
+        for row in reader:
+            res.setdefault(row['playlist_id'], list())
+            res[row['playlist_id']].append(row['track_id'])
+    return res
 
 
 def load_csv(path, dict_id):
@@ -28,3 +39,14 @@ def load_csv(path, dict_id):
             data[row[dict_id]] = {key: row[key]
                                   for key in fields}
     return data
+
+
+def build_train_final_matrix(dataset):
+    rows = max(map(int, dataset.train_final.keys()))
+    columns = max([max(map(int, x)) for x in dataset.train_final.values()])
+    M = lil_matrix((rows + 1, columns + 1))
+    print(M.shape)
+    for k, v in dataset.train_final.items():
+        for track in v:
+            M[k, track] = 1
+    return M
--- test.py
+++ test.py
@@ -1,13 +1,95 @@
 from loader import *
+from scipy.sparse import *
+import csv
+
+
+H = 10
+
+
+class Recommendation():
+    """docstring for Recommendation"""
+
+    def __init__(self):
+        self.track_id = ''
+        self.rating = 0.0
+
+    def __cmp__(self, object):
+        if object is None:
+            return -1
+        elif self.rating < object.rating:
+            return -2
+        elif self.rating == object.rating:
+            return 0
+        else:
+            return 1
+
+    def __lt__(self, object):
+        if object is None:
+            return False
+        else:
+            return self.rating < object.rating
+
+    def __eq__(self, object):
+        if object is None:
+            return False
+        else:
+            return self.rating == object.rating
+
+    def __str__(self):
+        return ('{' + self.track_id + ', ' + self.rating + '}')
 
 
 def main():
     dataset = Dataset()
-    print(dataset.playlists_final,
-          dataset.target_playlists,
-          dataset.tracks_final,
-          dataset.train_final,
-          dataset.target_tracks, sep='\n')
+    print('Dataset built.')
+    M = build_train_final_matrix(dataset)
+    print('Train final matrix built.')
+    M_csc = csc_matrix(M)
+    items_bias = lil_matrix((1, M_csc.shape[1]))
+
+    # Iterate over columns to compute items bias
+    for c_id in range(M_csc.shape[1]):
+        playlists = M_csc.data[M_csc.indptr[c_id]:M_csc.indptr[c_id + 1]]
+        # Items is a vector of ones, so its sum is equal to its size
+        cum = playlists.size
+        items_bias[0, c_id] = (cum / (cum + H))
+
+    # Normalize M_csc subtracting item bias
+    for c_id in range(M_csc.shape[1]):
+        M_csc.data[M_csc.indptr[c_id]:
+                   M_csc.indptr[c_id + 1]] -= items_bias[0, c_id]
+
+    print('Computed item bias and normalized URM')
+    M_csr = M_csc.tocsr()
+    users_bias = lil_matrix((M_csr.shape[0], 1))
+    # Iterate over rows to compute users bias
+    for r_id in range(M_csr.shape[0]):
+        tracks = M_csr.data[M_csr.indptr[r_id]:M_csr.indptr[r_id + 1]]
+        cum = tracks.size
+        users_bias[r_id, 0] = (cum / (cum + H))
+
+    print('Computed user bias')
+    # For each playlist in target_playlists
+    rec = {}
+    for pl_id in dataset.target_playlists.keys():
+        # For each track in target_tracks
+        rec.setdefault(pl_id, [Recommendation() for x in range(5)])
+        for t_id in dataset.target_tracks.keys():
+            if t_id not in dataset.train_final[pl_id]:
+                r_ui = Recommendation()
+                r_ui.rating = items_bias[0, int(
+                    t_id)] + users_bias[int(pl_id), 0]
+                r_ui.track_id = t_id
+                min_rec = min(rec[pl_id])
+                if r_ui > min_rec:
+                    ind = rec[pl_id].index(min_rec)
+                    rec[pl_id][ind] = r_ui
+
+    print('Recommendation done.')
+    with open('dict.csv', 'w') as csv_file:
+        writer = csv.writer(csv_file)
+        for k, v in rec:
+            writer.writerow([k, v])
 
 
 if __name__ == '__main__':
