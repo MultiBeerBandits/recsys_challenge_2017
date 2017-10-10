c0ad1fc2a176fcb6697b499a703531dbe1d83ebd
Emanuele Ghelfi <manughelfi1994@gmail.com>
2017-10-10 19:59:50 +0200

    Bug fixing, explore the whole space of parameters

--- test_item_based_filtering.py
+++ test_item_based_filtering.py
@@ -7,60 +7,41 @@ from item_based_filtering import *
 def main():
     best_map = 0
     best_shrinkage = 0
+    best_k_filtering = 0
     for shr in range(10, 150, 10):
-        ds = Dataset()
-        ev = evaluator.Evaluator()
-        ev.cross_validation(5, ds.train_final.copy())
-        ubf = ItemBasedFiltering()
-        for i in range(0, 5):
-            urm, tg_tracks, tg_playlist = ev.get_fold(ds)
-            ubf.fit(urm, tg_playlist, tg_tracks, ds, shr)
-            recs = ubf.predict()
-            ev.evaluate_fold(recs)
-        map_at_five = ev.get_mean_map()
-        print("MAP@5 [Shrinkage = " + str(shr) + ']', map_at_five)
-        # Store best shrinkage factor according to highest MAP
-        if map_at_five > best_map:
-            best_map = map_at_five
-            best_shrinkage = shr
+        for k_f in range(20, 100, 5):
+            ds = Dataset()
+            ev = evaluator.Evaluator()
+            ev.cross_validation(5, ds.train_final.copy())
+            ibf = ItemBasedFiltering()
+            for i in range(0, 5):
+                urm, tg_tracks, tg_playlist = ev.get_fold(ds)
+                ibf.fit(urm, tg_playlist, tg_tracks, ds, shr, k_f)
+                recs = ibf.predict()
+                ev.evaluate_fold(recs)
+            map_at_five = ev.get_mean_map()
+            print("MAP@5 [Shrinkage = " + str(shr) +  ' K_f ' + str(k_f) + ']', map_at_five)
+            # Store best shrinkage factor according to highest MAP
+            if map_at_five > best_map:
+                best_map = map_at_five
+                best_shrinkage = shr
+                best_k_filtering = k_f
     print(('Best MAP@5 = ' + str(best_map) +
            ' [Shrinkage factor:' + str(best_shrinkage) +
-           ', Default K_filtering]'))
-    best_map = 0
-    best_k_filtering = 0
-    for k_f in range(20, 55, 5):
-        ds = Dataset()
-        ev = evaluator.Evaluator()
-        ev.cross_validation(5, ds.train_final.copy())
-        ubf = ItemBasedFiltering()
-        for i in range(0, 5):
-            urm, tg_tracks, tg_playlist = ev.get_fold(ds)
-            ubf.fit(urm, tg_playlist, tg_tracks, ds, best_shrinkage, k_f)
-            recs = ubf.predict()
-            ev.evaluate_fold(recs)
-        map_at_five = ev.get_mean_map()
-        print("MAP@5 [K_filtering = " + str(k_f) + ']', map_at_five)
-        # Store best shrinkage factor according to highest MAP
-        if map_at_five > best_map:
-            best_map = map_at_five
-            best_k_filtering = k_f
-    print(('Best MAP@5 = ' + str(best_map) +
-           ' [Shrinkage factor:' + str(best_shrinkage) + ', K_filtering: ' +
-           str(best_k_filtering) + ']'))
-
+           ', K_f ' + str(best_k_filtering) + ']'))
     # export csv
-    ubf_exporter = ItemBasedFiltering()
+    ibf_exporter = ItemBasedFiltering()
     urm = ds.build_train_matrix()
     tg_playlist = list(ds.target_playlists.keys())
     tg_tracks = list(ds.target_tracks.keys())
     # Train the model with the best shrinkage found in cross-validation
-    ubf_exporter.fit(urm,
+    ibf_exporter.fit(urm,
                      tg_playlist,
                      tg_tracks,
                      ds,
                      best_shrinkage,
                      best_k_filtering)
-    recs = ubf_exporter.predict()
+    recs = ibf_exporter.predict()
     with open('submission_ubf.csv', mode='w', newline='') as out:
         fieldnames = ['playlist_id', 'track_ids']
         writer = csv.DictWriter(out, fieldnames=fieldnames, delimiter=',')
--- test_user_based.py
+++ test_user_based.py
@@ -1,49 +1,32 @@
 from loader_v2 import *
 from scipy.sparse import *
 import evaluator
-from local_user_based import *
+from user_based import *
 
 
 def main():
     best_map = 0
     best_shrinkage = 0
-    for shr in range(10, 150, 10):
-        ds = Dataset()
-        ev = evaluator.Evaluator()
-        ev.cross_validation(5, ds.train_final.copy())
-        ubf = UserBasedFiltering()
-        for i in range(0, 5):
-            urm, tg_tracks, tg_playlist = ev.get_fold(ds)
-            ubf.fit(urm, tg_playlist, tg_tracks, ds, shr)
-            recs = ubf.predict()
-            ev.evaluate_fold(recs)
-        map_at_five = ev.get_mean_map()
-        print("MAP@5 [Shrinkage = " + str(shr) + ']', map_at_five)
-        # Store best shrinkage factor according to highest MAP
-        if map_at_five > best_map:
-            best_map = map_at_five
-            best_shrinkage = shr
-    print(('Best MAP@5 = ' + str(best_map) +
-           ' [Shrinkage factor:' + str(best_shrinkage) +
-           ', Default K_filtering]'))
-    best_map = 0
     best_k_filtering = 0
-    for k_f in range(20, 55, 5):
-        ds = Dataset()
-        ev = evaluator.Evaluator()
-        ev.cross_validation(5, ds.train_final.copy())
-        ubf = UserBasedFiltering()
-        for i in range(0, 5):
-            urm, tg_tracks, tg_playlist = ev.get_fold(ds)
-            ubf.fit(urm, tg_playlist, tg_tracks, ds, best_shrinkage, k_f)
-            recs = ubf.predict()
-            ev.evaluate_fold(recs)
-        map_at_five = ev.get_mean_map()
-        print("MAP@5 [K_filtering = " + str(k_f) + ']', map_at_five)
-        # Store best shrinkage factor according to highest MAP
-        if map_at_five > best_map:
-            best_map = map_at_five
-            best_k_filtering = k_f
+    for shr in range(10, 150, 10):
+        for k_f in range(20, 100, 5):
+            ds = Dataset()
+            ev = evaluator.Evaluator()
+            ev.cross_validation(5, ds.train_final.copy())
+            ubf = UserBasedFiltering()
+            for i in range(0, 5):
+                urm, tg_tracks, tg_playlist = ev.get_fold(ds)
+                ubf.fit(urm, tg_playlist, tg_tracks, ds, shr, k_f)
+                recs = ubf.predict()
+                ev.evaluate_fold(recs)
+            map_at_five = ev.get_mean_map()
+            print("MAP@5 [Shrinkage = " + str(shr) +  ' K_f ' + str(k_f) + ']', map_at_five)
+            # Store best shrinkage factor according to highest MAP
+            if map_at_five > best_map:
+                best_map = map_at_five
+                best_shrinkage = shr
+                best_k_filtering = k_f
+
     print(('Best MAP@5 = ' + str(best_map) +
            ' [Shrinkage factor:' + str(best_shrinkage) + ', K_filtering: ' +
            str(best_k_filtering) + ']'))
@@ -61,7 +44,7 @@ def main():
                      best_shrinkage,
                      best_k_filtering)
     recs = ubf_exporter.predict()
-    with open('submission_ubf.csv', mode='w', newline='') as out:
+    with open('submission_user_based_filtering.csv', mode='w', newline='') as out:
         fieldnames = ['playlist_id', 'track_ids']
         writer = csv.DictWriter(out, fieldnames=fieldnames, delimiter=',')
         writer.writeheader()
