20c14f6c47cfa6291762fc31932b6966125846f1
Emanuele Ghelfi <manughelfi1994@gmail.com>
2017-10-10 05:39:17 +0200

    Created class Evaluator

--- evaluator.py
+++ evaluator.py
@@ -2,114 +2,117 @@ from scipy.sparse import *
 import random
 import math
 
-# for each fold the dictionary of predictions
-test_dictionaries = []
 
-# target tracks is a list of lists, for each fold its target tracks
-target_tracks = []
+class Evaluator(object):
 
-# min number of tracks for playlists
-min_playlist_size = 10
+    # for each fold the dictionary of predictions
+    test_dictionaries = []
 
-# starts from -1 since get_fold add 1 at the beginning
-current_fold_index = -1
+    # target tracks is a list of lists, for each fold its target tracks
+    target_tracks = []
 
-# evaluation for each fold
-evaluations = []
+    # min number of tracks for playlists
+    min_playlist_size = 10
+
+    # starts from -1 since get_fold adds 1 at the beginning
+    current_fold_index = -1
+
+    # evaluation for each fold
+    evaluations = []
+
+    folds = 0
+
+    def cross_validation(self, folds, train_dataset):
+        """
+        Method for initializing the cross validation
+        folds is the number of folds in which the dataset is divided.
+        train dataset is a dict: {"playlist_id": [tracks]}
+        This method creates folds dictionaries for testing.
+        You have to call get_fold(fold_number, urm)
+        to get the urm ready for one iteration of training
+        """
+        self.folds = folds
+        # initialize test dictionary
+        for i in range(0, folds):
+            self.test_dictionaries.append({})
+            self.target_tracks.append(list())
+            self.evaluations.append(0)
+
+        # get the size of training set
+        training_set_size = sum(
+            [1 for x in train_dataset.values() if len(x) >= min_playlist_size])
+
+        fold_size = math.ceil(training_set_size / folds)
+
+        # get the playlist with at least min_playlist_size elements
+        playlists = [x for x in train_dataset.keys() if len(
+            train_dataset[x]) >= min_playlist_size]
+
+        # shuffle the list of playlists
+        random.shuffle(playlists)
+
+        # sublists is a list of lists. contains the folds
+        sublists = [playlists[x:x + fold_size]
+                    for x in range(0, len(playlists), fold_size)]
+        sample_number = 5
+
+        # sublist is a list of playlist, sample 5 tracks for each playlist
+        fold_index = 0
+        for sublist in sublists:
+            current_tg_tracks = set()
+            for pl in sublist:
+                # sample from the tracks of that playlist in train dataset
+                samples = random.sample(train_dataset[pl], sample_number)
+                # add tracks to the correct fold
+                self.test_dictionaries[fold_index][pl] = samples
+                current_tg_tracks = current_tg_tracks.union(samples)
+            self.target_tracks[fold_index] = list(current_tg_tracks)
+            fold_index += 1
+
+    def get_fold(self, dataset):
+        """
+        dataset is Dataset object
+        Returns:
+        the user rating matrix of the current fold, the target tracks
+        and target playlist
+        """
+        self.current_fold_index = self.current_fold_index + 1
+        urm = dataset.build_train_matrix()
+        current_fold = urm
+        for k in self.test_dictionaries[self.current_fold_index].keys():
+            playlist_index = dataset.get_playlist_index_from_id(k)
+            for track in self.test_dictionaries[self.current_fold_index][k]:
+                track_index = dataset.get_track_index_from_id(track)
+                current_fold[playlist_index, track_index] = 0
+        return current_fold, self.target_tracks[self.current_fold_index], self.test_dictionaries[self.current_fold_index].keys()
+
+    def evaluate_fold(self, recommendation):
+        """
+        recommendation is the dictionary of recommendation {'playlist ': list}
+        For each playlist in test_dictionary[current_fold] evaluate MAP
+        """
+        if self.current_fold_index < self.folds:
+            cumulated_ap = 0
+            for pl_id in self.test_dictionaries[self.current_fold_index].keys():
+                # avg precision
+                ap = 0
+                item_number = 1
+                relevant_items = 0
+                for tr_id in recommendation[pl_id]:
+                    if tr_id in self.test_dictionaries[self.current_fold_index][pl_id]:
+                        relevant_items += 1
+                        precision = relevant_items / item_number
+                        ap = ap + precision
+                    item_number += 1
+                cumulated_ap = cumulated_ap + ap
+            map_at_five = cumulated_ap / \
+                len(test_dictionaries[self.current_fold_index].keys())
+            print("MAP@5: " + str(map_at_five))
+            self.evaluations[self.current_fold_index] = map_at_five
 
 
-def cross_validation(folds, train_dataset):
-    """
-    Method for initializing the cross validation
-    folds is the number of folds in which the dataset is divided.
-    train dataset is a dict: {"playlist_id": [tracks]}
-    This method creates folds dictionaries for testing.
-    You have to call get_fold(fold_number, urm)
-    to get the urm ready for one iteration of training
-    """
-    # initialize test dictionary
-    for i in range(0, folds):
-        test_dictionaries.append({})
-        target_tracks.append(list())
-        evaluations.append(0)
-
-    # get the size of training set
-    training_set_size = sum(
-        [1 for x in train_dataset.values() if len(x) >= min_playlist_size])
-
-    fold_size = math.ceil(training_set_size / folds)
-
-    # get the playlist with at least min_playlist_size elements
-    playlists = [x for x in train_dataset.keys() if len(
-        train_dataset[x]) >= min_playlist_size]
-
-    # shuffle the list of playlists
-    random.shuffle(playlists)
-
-    # sublists is a list of lists. contains the folds
-    sublists = [playlists[x:x + fold_size]
-                for x in range(0, len(playlists), fold_size)]
-    sample_number = 5
-
-    # sublist is a list of playlist, sample 5 tracks for each playlist
-    fold_index = 0
-    for sublist in sublists:
-        current_tg_tracks = set()
-        for pl in sublist:
-            # sample from the tracks of that playlist in train dataset
-            samples = random.sample(train_dataset[pl], sample_number)
-            # add tracks to the correct fold
-            test_dictionaries[fold_index][pl] = samples
-            current_tg_tracks = current_tg_tracks.union(samples)
-        target_tracks[fold_index] = list(current_tg_tracks)
-        fold_index += 1
-
-
-def get_fold(dataset):
-    """
-    dataset is Dataset object
-    Returns:
-    the user rating matrix of the current fold, the target tracks
-    and target playlist
-    """
-    global current_fold_index
-    current_fold_index = current_fold_index + 1
-    urm = dataset.build_train_matrix()
-    current_fold = urm
-    for k in test_dictionaries[current_fold_index].keys():
-        playlist_index = dataset.get_playlist_index_from_id(k)
-        for track in test_dictionaries[current_fold_index][k]:
-            track_index = dataset.get_track_index_from_id(track)
-            current_fold[playlist_index, track_index] = 0
-    return current_fold, target_tracks[current_fold_index], test_dictionaries[current_fold_index].keys()
-
-
-def evaluate_fold(recommendation):
-    """
-    recommendation is the dictionary of recommendation {'playlist ': list}
-    For each playlist in test_dictionary[current_fold] evaluate MAP
-    """
-    cumulated_ap = 0
-    for pl_id in test_dictionaries[current_fold_index].keys():
-        # avg precision
-        ap = 0
-        item_number = 1
-        relevant_items = 0
-        for tr_id in recommendation[pl_id]:
-            if tr_id in test_dictionaries[current_fold_index][pl_id]:
-                relevant_items += 1
-                precision = relevant_items / item_number
-                ap = ap + precision
-            item_number += 1
-        cumulated_ap = cumulated_ap + ap
-    map_at_five = cumulated_ap / \
-        len(test_dictionaries[current_fold_index].keys())
-    print("MAP@5: " + str(map_at_five))
-    evaluations[current_fold_index] = map_at_five
-
-
-def get_mean_map():
-    """
-    Returns the mean map computed over each fold
-    """
-    return sum(evaluations) / len(evaluations)
+    def get_mean_map(self):
+        """
+        Returns the mean map computed over each fold
+        """
+        return sum(self.evaluations) / self.folds
--- test_cbf.py
+++ test_cbf.py
@@ -12,51 +12,53 @@ from time import gmtime, strftime
 def main():
     # number of values of each attributes
     n_values = 5
-    artist_weights = np.linspace(0.1, 1, n_values)
-    album_weights = np.linspace(0.1, 1, n_values)
-    duration_weights = np.linspace(0.1, 1, n_values)
-    playcount_weights = np.linspace(0.1, 1, n_values)
+    artist_weights = np.linspace(0, 1, n_values)
+    album_weights = np.linspace(0, 1, n_values)
+    duration_weights = np.linspace(0, 1, n_values)
+    playcount_weights = np.linspace(0, 1, n_values)
 
     # iterate over values
     for art_v in artist_weights:
         for alb_v in album_weights:
             for dur_v in duration_weights:
                 for pl_v in playcount_weights:
-                    weight_list = list((art_v, alb_v, dur_v, pl_v))
-                    print("Trying weights: ", weight_list)
-                    ds = Dataset()
-                    evaluator.cross_validation(5, ds.train_final)
-                    cbf = ContentBasedFiltering(weight_list)
-                    for i in range(0, 5):
-                        urm, tg_tracks, tg_playlist = evaluator.get_fold(ds)
-                        cbf.fit(urm, tg_playlist, tg_tracks, ds)
-                        recs = cbf.predict()
-                        evaluator.evaluate_fold(recs)
-                    map_at_five = evaluator.get_mean_map()
-                    print("MAP@5", map_at_five, "With weights ", weight_list)
-                    csv_name = './cbf_subm/submission_cbf_' + strftime("%Y-%m-%d-%H-%M-%S", gmtime()) + '.csv'
-                    with open('cbf_cv_info.log', mode='a') as out_file:
-                        to_write = ' '.join([str(x) for x in weight_list])
-                        to_write = 'MAP@5=' + str(map_at_five) + 'Obtained with ' + to_write + ' written in ' + csv_name
-                        out_file.write(to_write)
-                        out_file.flush()
-                    # export csv
-                    cbf_exporter = ContentBasedFiltering(weight_list)
-                    urm = ds.build_train_matrix()
-                    tg_playlist = list(ds.target_playlists.keys())
-                    tg_tracks = list(ds.target_tracks.keys())
-                    cbf_exporter.fit(urm, tg_playlist, tg_tracks, ds)
-                    recs = cbf_exporter.predict()
-                    with open(csv_name, mode='w', newline='') as out:
-                        fieldnames = ['playlist_id', 'track_ids']
-                        writer = csv.DictWriter(out, fieldnames=fieldnames, delimiter=',')
-                        writer.writeheader()
-                        for k in tg_playlist:
-                            track_ids = ''
-                            for r in recs[k]:
-                                track_ids = track_ids + r + ' '
-                            writer.writerow({'playlist_id': k,
-                                             'track_ids': track_ids[:-1]})
+                    if art_v != 0 or alb_v != 0 or dur_v != 0 or pl_v != 0:
+                        weight_list = list((art_v, alb_v, dur_v, pl_v))
+                        print("Trying weights: ", weight_list)
+                        ds = Dataset()
+                        ev = evaluator.Evaluator()
+                        ev.cross_validation(5, ds.train_final)
+                        cbf = ContentBasedFiltering(weight_list)
+                        for i in range(0, 5):
+                            urm, tg_tracks, tg_playlist = ev.get_fold(ds)
+                            cbf.fit(urm, tg_playlist, tg_tracks, ds)
+                            recs = cbf.predict()
+                            ev.evaluate_fold(recs)
+                        map_at_five = ev.get_mean_map()
+                        print("MAP@5", map_at_five, "With weights ", weight_list)
+                        # csv_name = './cbf_subm/submission_cbf_' + strftime("%Y-%m-%d-%H-%M-%S", gmtime()) + '.csv'
+                        with open('cbf_cv_info.log', mode='a') as out_file:
+                            to_write = ' '.join([str(x) for x in weight_list])
+                            to_write = 'MAP@5=' + str(map_at_five) + 'Obtained with ' + to_write
+                            out_file.write(to_write)
+                            out_file.flush()
+                        # export csv
+                        # cbf_exporter = ContentBasedFiltering(weight_list)
+                        # urm = ds.build_train_matrix()
+                        # tg_playlist = list(ds.target_playlists.keys())
+                        # tg_tracks = list(ds.target_tracks.keys())
+                        # cbf_exporter.fit(urm, tg_playlist, tg_tracks, ds)
+                        # recs = cbf_exporter.predict()
+                        # with open(csv_name, mode='w', newline='') as out:
+                        #    fieldnames = ['playlist_id', 'track_ids']
+                        #    writer = csv.DictWriter(out, fieldnames=fieldnames, delimiter=',')
+                        #    writer.writeheader()
+                        #    for k in tg_playlist:
+                        #        track_ids = ''
+                        #        for r in recs[k]:
+                        #            track_ids = track_ids + r + ' '
+                        #        writer.writerow({'playlist_id': k,
+                        #                         'track_ids': track_ids[:-1]})
 
 
 if __name__ == '__main__':
