0e44c0d2a36f7eaac9944062d180e15074e4b835
Leonardo Arcari <lippol94@gmail.com>
2017-10-14 11:42:11 +0200

    Initial implementation of Non-Negative Matrix Factorization
    + Test script

---
+++ NMF.py
@@ -0,0 +1,80 @@
+from scipy.sparse import *
+import numpy as np
+
+
+class NMF():
+    """docstring for NMF"""
+
+    def __init__(self, urm, features, learning_steps):
+        # Number of latent factors
+        self.features = features
+
+        # Number of learning iterations
+        self.learning_steps = learning_steps
+
+        # Store a reference to URM
+        self.urm = urm
+
+        # User factor matrix
+        self.U = np.zeros((urm.shape[0], features))
+        self.U.fill(0.1)
+
+        # Item factor matrix
+        self.V = np.zeros((urm.shape[1], features))
+        self.V.fill(0.1)
+
+    def fit(self, u_reg, v_reg):
+        for i in range(self.learning_steps):
+            print(('[NMF][Fit with u_reg = ' + str(u_reg) +
+                   ', v_reg = ' + str(v_reg) + '] Step ' + str(i)))
+            # Update user factor matrix
+            u_num = self.urm.dot(self.V)
+            u_num = u_num - self.U * u_reg
+            u_den = self.V.transpose().dot(self.V)
+            u_den = self.U.dot(u_den)
+            u_den += 1e-10
+            U_prime = u_num / u_den
+            U_prime = np.multiply(U_prime, self.U)
+            U_prime[(U_prime < 0)] = 0
+            self.U = U_prime
+            print(('[NMF][Fit with u_reg = ' + str(u_reg) +
+                   ', v_reg = ' + str(v_reg) +
+                   '] User factor matrix updated...'))
+
+            # Update item factor matrix
+            v_num = self.urm.transpose().dot(self.U)
+            v_num = v_num - self.V * v_reg
+            v_den = self.U.transpose().dot(self.U)
+            v_den = self.V.dot(v_den)
+            v_den += 1e-10
+            v_den = np.reciprocal(v_den)
+            v_num = np.multiply(v_num, v_den)
+            v_num = np.multiply(v_num, self.V)
+            v_num[(v_num < 0)] = 0
+            self.V = v_num
+            print(('[NMF][Fit with u_reg = ' + str(u_reg) +
+                   ', v_reg = ' + str(v_reg) +
+                   '] Item factor matrix updated...'))
+
+    def predict(self, target_playlist, target_tracks, dataset, at=5):
+        self.U = self.U[[dataset.get_playlist_index_from_id(x)
+                         for x in target_playlist]]
+        self.V = self.V[[dataset.get_track_index_from_id(x)
+                         for x in target_tracks]]
+        self.urm = self.urm[[dataset.get_playlist_index_from_id(x)
+                             for x in target_playlist]]
+        self.urm = self.urm[:, [dataset.get_track_index_from_id(x)
+                                for x in target_tracks]]
+        R_hat = self.U.dot(self.V.transpose())
+        R_hat[(self.urm != 1)] = 0
+        R_hat = csr_matrix(R_hat)
+        recs = {}
+        for row_i in range(R_hat.shape[0]):
+            pl_id = target_playlist[row_i]
+            row = R_hat.data[R_hat.indptr[row_i]:R_hat.indptr[row_i + 1]]
+            sorted_mask = np.flip(row.argsort(), axis=0)[0:at]
+            track_cols = [R_hat.indices[R_hat.indptr[row_i] + x]
+                          for x in sorted_mask]
+            tracks_ids = [target_tracks[x] for x in track_cols]
+            recs[pl_id] = tracks_ids
+        return recs
---
+++ test_NMF.py
@@ -0,0 +1,41 @@
+from loader_v2 import *
+from scipy.sparse import *
+import evaluator
+from NMF import *
+
+
+def main():
+    ds = Dataset()
+    ev = evaluator.Evaluator()
+    ev.cross_validation(5, ds.train_final.copy())
+    for i in range(0, 5):
+        urm, tg_tracks, tg_playlist = ev.get_fold(ds)
+        nmf = NMF(urm, features=100, learning_steps=1000)
+        nmf.fit(0.02, 0.02)
+        recs = nmf.predict(tg_playlist, tg_tracks, ds)
+        ev.evaluate_fold(recs)
+    map_at_five = ev.get_mean_map()
+    print("MAP@5 [Features = 100, Iterations: 1000] ", map_at_five)
+
+    # export csv
+    urm = ds.build_train_matrix()
+    nmf_exporter = NMF(urm, features=100, learning_steps=1000)
+    tg_playlist = list(ds.target_playlists.keys())
+    tg_tracks = list(ds.target_tracks.keys())
+    # Train the model with the best shrinkage found in cross-validation
+    nmf_exporter.fit(0.02, 0.02)
+    recs = nmf_exporter.predict(tg_playlist, tg_tracks, ds)
+    with open('submission_nmf.csv', mode='w', newline='') as out:
+        fieldnames = ['playlist_id', 'track_ids']
+        writer = csv.DictWriter(out, fieldnames=fieldnames, delimiter=',')
+        writer.writeheader()
+        for k in tg_playlist:
+            track_ids = ''
+            for r in recs[k]:
+                track_ids = track_ids + r + ' '
+            writer.writerow({'playlist_id': k,
+                             'track_ids': track_ids[:-1]})
+
+
+if __name__ == '__main__':
+    main()
