f4a807d2000f36a572a33a818b5a26ff9cceef4e
Emanuele Ghelfi <manughelfi1994@gmail.com>
2017-12-07 14:29:30 +0100

    bug fix

--- src/FWUM/test_xsq.py
+++ src/FWUM/test_xsq.py
@@ -16,7 +16,7 @@ def main():
     for i in range(0, 5):
         urm, tg_tracks, tg_playlist = ev.get_fold(ds)
         xbf.fit(urm, tg_playlist, tg_tracks, ds)
-        cbf = cbf.fit(urm, tg_playlist, tg_tracks, ds)
+        cbf.fit(urm, tg_playlist, tg_tracks, ds)
         recs = xbf.predict()
         ev.evaluate_fold(recs)
         recs = cbf.predict()
